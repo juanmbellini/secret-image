a05bcfcd44d6aca6efc90a3f6ccbcfccc16ae015
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-06-23 19:55:26 -0300

    Add missing validation of integer values (k and n)
    - Include a custom PositiveIntegerValidator

--- src/main/java/ar/edu/itba/cripto/secret_image/main/Main.java
+++ src/main/java/ar/edu/itba/cripto/secret_image/main/Main.java
@@ -9,19 +9,28 @@ import com.beust.jcommander.ParameterException;
  */
 public class Main implements Runnable {
 
-    @Parameter(names = {"-h", "--help"}, description = "Print usage.", help = true)
+
+    // ========================================================
+    // Running parameters
+    // ========================================================
+
+    @Parameter(names = {"-h", "--help"},
+            description = "Print usage.",
+            help = true)
     private boolean usage = false;
 
     /**
      * Indicates whether the execution will provide shadow images.
      */
-    @Parameter(names = {"-d"}, description = "Run in distribution mode.")
+    @Parameter(names = {"-d"},
+            description = "Run in distribution mode.")
     private boolean distribution;
 
     /**
      * Indicates whether the execution will recover the secret image from shadow images.
      */
-    @Parameter(names = {"-r"}, description = "Run in recovery mode.")
+    @Parameter(names = {"-r"},
+            description = "Run in recovery mode.")
     private boolean recovery;
 
     /**
@@ -30,7 +39,8 @@ public class Main implements Runnable {
      * In case the execution is running in recovery mode, this will be the target path
      * (i.e where the recovered secret image will be saved)
      */
-    @Parameter(names = {"-secret"}, required = true,
+    @Parameter(names = {"-secret"},
+            required = true,
             description = "Path to secret image. In distribution mode, this must be the path" +
                     " to the secret image to distribute. In recovery mode, this must be the target path (i.e where " +
                     " the recovered secret image will be saved.")
@@ -39,8 +49,11 @@ public class Main implements Runnable {
     /**
      * Indicates the minimum amount of shadows that are needed to recover the secret image.
      */
-    @Parameter(names = {"-k"}, required = true,
-            description = "The minimum amount of shadows needed to recover the secret image.")
+    @Parameter(names = {"-k"},
+            required = true,
+            description = "The minimum amount of shadows needed to recover the secret image.",
+            validateWith = PositiveIntegerValidator.class
+    )
     private int minimumShadows;
 
     /**
@@ -49,24 +62,38 @@ public class Main implements Runnable {
      * If it is not set, default will be the amount of images in the shadows directory
      * (i.e {@link Main#shadowsDirectory}).
      */
-    @Parameter(names = {"-n"}, description = "Optional. The amount of shadows to be created." +
-            " Must be greater or equal than the minimum amount of shadows." +
-            " Must only be used in distribution mode." +
-            " If not set, the amount of images in the set directory (see -dir param) will be used.")
+    @Parameter(names = {"-n"},
+            description = "Optional. The amount of shadows to be created." +
+                    " Must be greater or equal than the minimum amount of shadows." +
+                    " Must only be used in distribution mode." +
+                    " If not set, the amount of images in the set directory (see -dir param) will be used.",
+            validateWith = PositiveIntegerValidator.class)
     private Integer amountOfShadows;
 
-    @Parameter(names = {"-dir"}, description = "Optional. The path to the directory containing the shadow images." +
-            " When running in distribution mode, the directory must contain the images in which the secret" +
-            " will be saved." +
-            " When running in recovery mode, the directory must contain the images with the hidden secret." +
-            " If not set, the current working directory will be used.")
+    @Parameter(names = {"-dir"},
+            description = "Optional. The path to the directory containing the shadow images." +
+                    " When running in distribution mode, the directory must contain the images in which the secret" +
+                    " will be saved." +
+                    " When running in recovery mode, the directory must contain the images with the hidden secret." +
+                    " If not set, the current working directory will be used.")
     private String shadowsDirectory = "./";
+
+
+    // ========================================================
+    // Main class instance variables
+    // ========================================================
 
 
     /**
      * A {@link JCommander} instance for parsing execution parameters.
      */
     private final JCommander jCommander;
+
+
+    // ========================================================
+    // Constructor and methods
+    // ========================================================
+
 
     /**
      * Constructor.
@@ -79,17 +106,15 @@ public class Main implements Runnable {
         this.jCommander.parse(args);
     }
 
-
     @Override
     public void run() {
         if (this.usage) {
             this.jCommander.usage();
             return;
         }
-        System.out.println("Hello world!");
         this.validateParameters();
+        System.out.println("Hello world!");
     }
-
 
     /**
      * Performs global parameters validation.
@@ -112,6 +137,11 @@ public class Main implements Runnable {
     }
 
 
+    // ========================================================
+    // Main execution
+    // ========================================================
+
+
     /**
      * Entry point.
      *
@@ -119,9 +149,8 @@ public class Main implements Runnable {
      */
     public static void main(String[] args) {
 
-        Main main = new Main(args);
         try {
-            main.run();
+            new Main(args).run();
         } catch (Throwable e) {
             System.err.println(e.getMessage());
             System.err.println("Problems were encountered while executing system.");
---
+++ src/main/java/ar/edu/itba/cripto/secret_image/main/PositiveIntegerValidator.java
@@ -0,0 +1,20 @@
+package ar.edu.itba.cripto.secret_image.main;
+
+import com.beust.jcommander.ParameterException;
+import com.beust.jcommander.validators.PositiveInteger;
+
+/**
+ * An extension of {@link PositiveInteger} parameter validator,
+ * in which the {@link ParameterException} thrown has its message changed.
+ */
+public class PositiveIntegerValidator extends PositiveInteger {
+
+    @Override
+    public void validate(String name, String value) throws ParameterException {
+        try {
+            super.validate(name, value);
+        } catch (ParameterException e) {
+            throw new ParameterException("Fatal. " + e.getMessage());
+        }
+    }
+}

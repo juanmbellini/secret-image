d971dfbdbd508281aa1fd6ed3f4ec7ff8962f93a
lelv <imlelv@gmail.com>
2017-06-24 18:33:42 -0300

    Added BmpEditor

--- src/main/java/ar/edu/itba/cripto/secret_image/Main.java
+++ src/main/java/ar/edu/itba/cripto/secret_image/Main.java
@@ -1,8 +1,10 @@
 package ar.edu.itba.cripto.secret_image;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+import ar.edu.itba.cripto.secret_image.bmp.BmpEditor;
+import ar.edu.itba.cripto.secret_image.bmp.BmpUtils;
+
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Hello world!
@@ -11,8 +13,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
 
-        final BufferedImage image = ImageIO.read(Main.class.getResourceAsStream("/shadows/1.bmp"));
+        //final BufferedImage image = ImageIO.read(Main.class.getResourceAsStream("/shadows/1.bmp"));
+        BmpUtils bmpUtils = new BmpUtils("/home/lelv/Documents/cripto/EjemploSinSecretoSolobmp/Facundo.bmp",1);
+        BmpEditor editor = bmpUtils.edit();
 
+        editor.insertSecret(129);
     }
 
 

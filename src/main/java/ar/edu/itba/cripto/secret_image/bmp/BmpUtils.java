d6e7db2bf1293af8ed97e5aa795f3436bec3e09f
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-06-26 14:11:45 -0300

    Add javadocs to BmpUtils

--- src/main/java/ar/edu/itba/cripto/secret_image/bmp/BmpUtils.java
+++ src/main/java/ar/edu/itba/cripto/secret_image/bmp/BmpUtils.java
@@ -14,21 +14,62 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class BmpUtils implements Iterable<List<Integer>>{
+/**
+ * Class representing a bmp image.
+ */
+public class BmpUtils implements Iterable<List<Integer>> {
 
-    //Metadata
+    // ================================
+    // Metadata
+    // ================================
+    /**
+     * The file size.
+     */
     private final int fileSize;
+    /**
+     * The shadow.
+     */
     private final int shadow;
+    /**
+     * The seed.
+     */
     private final int seed;
+    /**
+     * The image width.
+     */
     private final int width;
-    private final int heigth;
+    /**
+     * The image height.
+     */
+    private final int height;
+    /**
+     * The image offset (i.e where the real data starts).
+     */
     /*package*/ final int offset;
 
-    //Constructor
+    // ================================
+    // Internal data
+    // ================================
+    /**
+     * The real {@link File} that represents this image.
+     */
     /*package*/ final File file;
+    /**
+     * The array of bytes representing the image's raw data.
+     */
     /*package*/ final byte[] fileBytes;
+
+    /**
+     * The amount of bytes that will be returned each time this image is iterated.
+     */
     private int bytesFromIterator = 8;
 
+    /**
+     * Constructor
+     *
+     * @param path Path where the image must be opened from.
+     * @throws IOException If any IO error occurs while opening/reading file.
+     */
     public BmpUtils(String path) throws IOException {
         this.file = new File(path);
 
@@ -46,9 +87,9 @@ public class BmpUtils implements Iterable<List<Integer>>{
         this.offset = (int) imageStream.readUnsignedInt();
         //Skip Length of BitMapInfoHeader
         imageStream.skipBytes(4);
-        //Width & heigth
-        this.width = (int)imageStream.readUnsignedInt();
-        this.heigth = (int)imageStream.readUnsignedInt();
+        //Width & height
+        this.width = (int) imageStream.readUnsignedInt();
+        this.height = (int) imageStream.readUnsignedInt();
 
         imageStream.close();
 
@@ -58,38 +99,80 @@ public class BmpUtils implements Iterable<List<Integer>>{
 
     }
 
-    /* package */ BmpUtils(File file, byte[] bytes, BmpUtils shadow, int heigth){
+    /**
+     * Constructor.
+     *
+     * @param file   The real {@link File} that represents this image.
+     * @param bytes  The array of bytes representing the image's raw data.
+     * @param shadow A {@link BmpUtils} used as a reference (i.e data is taken from here).
+     * @param height The image height.
+     */
+    /* package */ BmpUtils(File file, byte[] bytes, BmpUtils shadow, int height) {
         this.file = file;
         this.fileBytes = bytes;
         this.fileSize = bytes.length;
         this.offset = shadow.offset;
-        this.seed=0;
-        this.shadow=0;
-        this.width=shadow.width;
-        this.heigth=heigth;
+        this.seed = 0;
+        this.shadow = 0;
+        this.width = shadow.width;
+        this.height = height;
     }
 
+    /**
+     * @return The file size.
+     */
     public long getFileSize() {
         return fileSize;
     }
 
-    public long getImageSize(){
-        return fileSize-offset;
+    /**
+     * @return The real image size (i.e the amount of pixels).
+     */
+    public long getImageSize() {
+        return fileSize - offset;
     }
 
+    /**
+     * @return The shadow.
+     */
     public int getShadow() {
         return shadow;
     }
 
+    /**
+     * @return The seed.
+     */
     public int getSeed() {
         return seed;
     }
 
+    /**
+     * @return The image width.
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * @return The image height.
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * Sets the amount of bytes that will be returned each time this image is iterated.
+     *
+     * @param bytesFromIterator The amount of bytes that will be returned each time this image is iterated.
+     */
     public void setBytesFromIterator(int bytesFromIterator) {
         this.bytesFromIterator = bytesFromIterator;
     }
 
-    public BmpEditor edit(){
+    /**
+     * @return A {@link BmpEditor} to edit this {@link BmpUtils}.
+     */
+    public BmpEditor edit() {
         return new BmpEditor(this);
     }
 
@@ -98,16 +181,15 @@ public class BmpUtils implements Iterable<List<Integer>>{
         return new KByteIterator();
     }
 
-    public int getWidth() {
-        return width;
-    }
 
-    public int getHeigth(){
-        return heigth;
-    }
-
+    /**
+     * An {@link Iterator} for a {@link BmpUtils}, iterating through the image real data, by blocks.
+     */
     private class KByteIterator implements Iterator<List<Integer>> {
 
+        /**
+         * The actual start of block.
+         */
         int index = offset;
 
         @Override
